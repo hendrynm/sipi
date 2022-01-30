0be271b553d4454d888a2feb4761cea056ff2261
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2022-01-30 18:51:17 +0700

    capaian provinsi done

--- resources/views/components/puskesmas-form.blade.php
+++ resources/views/components/puskesmas-form.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    <label for="puskesmasForm">Kabupaten</label>
+    <label for="puskesmasForm">Puskesmas</label>
     <select required class="form-control" name="puskesmasForm">
         @foreach($puskesmas as $pus)
             <option value="{{$pus->id_puskesmas}}" {{$pus->id_puskesmas == (int)$puskesmasForm? 'selected': ''}}>{{$pus->nama_puskesmas}}</option>
