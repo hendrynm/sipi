1e80dfc7db5aa19893a247ea87f24d4c2bb22798
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2022-01-31 20:45:23 +0700

    desa uci provinsi, kabupaten, puskesmas done

--- app/Http/Controllers/ProvinsiCapaianController.php
+++ app/Http/Controllers/ProvinsiCapaianController.php
@@ -7,6 +7,7 @@ use App\Models\KabupatenModel;
 use App\Models\PuskesmasModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\DataTables;
 
 class ProvinsiCapaianController extends Controller
 {
@@ -6083,5 +6084,83 @@ class ProvinsiCapaianController extends Controller
     ");
 
         return view('provinsi.capaian.capaianT', ['tahunForm' => $tahunForm, 'query' => $query, 'query1' => $query1, 'query2' => $query2]);
+    }
+
+    public function capaianUCI(Request $request) {
+        $tahunForm = $request->tahunForm ?: 2020;
+
+        return view('provinsi.capaian.capaianUCI', ['tahunForm' => $tahunForm]);
+    }
+
+    public function getListUciByQuarterId($year, $quarter) {
+        $tahunForm = $year;
+        $query = "";
+
+        if ($quarter == 1) {
+            $query = "SELECT kabupaten.nama_kabupaten as kabupaten,
+	puskesmas.nama_puskesmas as puskesmas,
+    kampung.nama_kampung as kampung,
+    SUM(CASE WHEN data_individu.idl = 1 AND (MONTH(data_individu.tanggal_idl) = 01 OR MONTH(data_individu.tanggal_idl) = 02 OR MONTH(data_individu.tanggal_idl) = 03) AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END) as idl,
+    ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.2) as sasaran,
+    ROUND((SUM(CASE WHEN data_individu.idl = 1 AND (MONTH(data_individu.tanggal_idl) = 01 OR MONTH(data_individu.tanggal_idl) = 02 OR MONTH(data_individu.tanggal_idl) = 03) AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END))/(ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.2))*100) as ketercapaian,
+    (CASE WHEN ((SUM(CASE WHEN data_individu.idl = 1 AND (MONTH(data_individu.tanggal_idl) = 01 OR MONTH(data_individu.tanggal_idl) = 02 OR MONTH(data_individu.tanggal_idl) = 03) AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END))/(ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.2))*100) > 20 THEN 'UCI' ELSE 'Non-UCI' END) as uci
+        FROM kampung
+          LEFT JOIN data_individu ON data_individu.id_kampung = kampung.id_kampung
+          LEFT JOIN puskesmas ON puskesmas.id_puskesmas = kampung.id_puskesmas
+          LEFT JOIN kabupaten ON kabupaten.id_kabupaten = puskesmas.id_kabupaten
+        GROUP BY kampung.id_kampung
+        ORDER BY kampung.id_kampung";
+        }
+        elseif  ($quarter == 2) {
+            $query = "SELECT kabupaten.nama_kabupaten as kabupaten,
+	puskesmas.nama_puskesmas as puskesmas,
+    kampung.nama_kampung as kampung,
+    SUM(CASE WHEN data_individu.idl = 1 AND (MONTH(data_individu.tanggal_idl) = 01 OR MONTH(data_individu.tanggal_idl) = 02 OR MONTH(data_individu.tanggal_idl) = 03 OR MONTH(data_individu.tanggal_idl) = 04 OR MONTH(data_individu.tanggal_idl) = 05 OR MONTH(data_individu.tanggal_idl) = 06) AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END) as idl,
+    ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.4) as sasaran,
+    ROUND((SUM(CASE WHEN data_individu.idl = 1 AND (MONTH(data_individu.tanggal_idl) = 01 OR MONTH(data_individu.tanggal_idl) = 02 OR MONTH(data_individu.tanggal_idl) = 03 OR MONTH(data_individu.tanggal_idl) = 04 OR MONTH(data_individu.tanggal_idl) = 05 OR MONTH(data_individu.tanggal_idl) = 06) AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END))/(ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.4))*100) as ketercapaian,
+    (CASE WHEN ((SUM(CASE WHEN data_individu.idl = 1 AND (MONTH(data_individu.tanggal_idl) = 01 OR MONTH(data_individu.tanggal_idl) = 02 OR MONTH(data_individu.tanggal_idl) = 03 OR MONTH(data_individu.tanggal_idl) = 04 OR MONTH(data_individu.tanggal_idl) = 05 OR MONTH(data_individu.tanggal_idl) = 06) AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END))/(ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.4))*100) > 40 THEN 'UCI' ELSE 'Non-UCI' END) as uci
+        FROM kampung
+          LEFT JOIN data_individu ON data_individu.id_kampung = kampung.id_kampung
+          LEFT JOIN puskesmas ON puskesmas.id_puskesmas = kampung.id_puskesmas
+          LEFT JOIN kabupaten ON kabupaten.id_kabupaten = puskesmas.id_kabupaten
+        GROUP BY kampung.id_kampung
+        ORDER BY kampung.id_kampung";
+        }
+        elseif ($quarter == 3) {
+            $query = "SELECT kabupaten.nama_kabupaten as kabupaten,
+	puskesmas.nama_puskesmas as puskesmas,
+    kampung.nama_kampung as kampung,
+    SUM(CASE WHEN data_individu.idl = 1 AND (MONTH(data_individu.tanggal_idl) = 01 OR MONTH(data_individu.tanggal_idl) = 02 OR MONTH(data_individu.tanggal_idl) = 03 OR MONTH(data_individu.tanggal_idl) = 04 OR MONTH(data_individu.tanggal_idl) = 05 OR MONTH(data_individu.tanggal_idl) = 06 OR MONTH(data_individu.tanggal_idl) = 07 OR MONTH(data_individu.tanggal_idl) = 08 OR MONTH(data_individu.tanggal_idl) = 09) AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END) as idl,
+    ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.6) as sasaran,
+    ROUND((SUM(CASE WHEN data_individu.idl = 1 AND (MONTH(data_individu.tanggal_idl) = 01 OR MONTH(data_individu.tanggal_idl) = 02 OR MONTH(data_individu.tanggal_idl) = 03 OR MONTH(data_individu.tanggal_idl) = 04 OR MONTH(data_individu.tanggal_idl) = 05 OR MONTH(data_individu.tanggal_idl) = 06 OR MONTH(data_individu.tanggal_idl) = 07 OR MONTH(data_individu.tanggal_idl) = 08 OR MONTH(data_individu.tanggal_idl) = 09) AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END))/(ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.6))*100) as ketercapaian,
+    (CASE WHEN ((SUM(CASE WHEN data_individu.idl = 1 AND (MONTH(data_individu.tanggal_idl) = 01 OR MONTH(data_individu.tanggal_idl) = 02 OR MONTH(data_individu.tanggal_idl) = 03 OR MONTH(data_individu.tanggal_idl) = 04 OR MONTH(data_individu.tanggal_idl) = 05 OR MONTH(data_individu.tanggal_idl) = 06 OR MONTH(data_individu.tanggal_idl) = 07 OR MONTH(data_individu.tanggal_idl) = 08 OR MONTH(data_individu.tanggal_idl) = 09) AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END))/(ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.6))*100) > 60 THEN 'UCI' ELSE 'Non-UCI' END) as uci
+        FROM kampung
+          LEFT JOIN data_individu ON data_individu.id_kampung = kampung.id_kampung
+          LEFT JOIN puskesmas ON puskesmas.id_puskesmas = kampung.id_puskesmas
+          LEFT JOIN kabupaten ON kabupaten.id_kabupaten = puskesmas.id_kabupaten
+        GROUP BY kampung.id_kampung
+        ORDER BY kampung.id_kampung";
+        }
+        elseif ($quarter == 4) {
+            $query = "SELECT kabupaten.nama_kabupaten as kabupaten,
+	puskesmas.nama_puskesmas as puskesmas,
+    kampung.nama_kampung as kampung,
+    SUM(CASE WHEN data_individu.idl = 1 AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END) as idl,
+    ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.8) as sasaran,
+    ROUND((SUM(CASE WHEN data_individu.idl = 1 AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END))/(ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.8))*100) as ketercapaian,
+    (CASE WHEN ((SUM(CASE WHEN data_individu.idl = 1 AND YEAR(data_individu.tanggal_idl) = {$tahunForm} THEN 1 ELSE 0 END))/(ROUND((kampung.surviving_infant_L + kampung.surviving_infant_P)*0.8))*100) > 80 THEN 'UCI' ELSE 'Non-UCI' END) as uci
+        FROM kampung
+          LEFT JOIN data_individu ON data_individu.id_kampung = kampung.id_kampung
+          LEFT JOIN puskesmas ON puskesmas.id_puskesmas = kampung.id_puskesmas
+          LEFT JOIN kabupaten ON kabupaten.id_kabupaten = puskesmas.id_kabupaten
+        GROUP BY kampung.id_kampung
+        ORDER BY kampung.id_kampung";
+        }
+
+        $data = DB::select($query);
+        return Datatables::of($data)
+            ->addIndexColumn()
+            ->make(true);
+
     }
 }
