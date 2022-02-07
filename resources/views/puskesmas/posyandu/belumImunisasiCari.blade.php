6eb360f99a74647c01631554059f9b92569388ec
Hendry Naufal Marbella <hendrynm@users.noreply.github.com>
2022-02-07 21:28:59 +0700

    Fix print laporan antigen

--- resources/views/puskesmas/posyandu/belumImunisasiCari.blade.php
+++ resources/views/puskesmas/posyandu/belumImunisasiCari.blade.php
@@ -36,56 +36,56 @@
             dataType: 'json',
             cache: false,
             success: function(data) {
+                $(".nama").append(data.nama_antigen);
                 let nama = data.nama_antigen;
-                $(".nama").append(nama);
-            }
-        });
 
-        $(function () {
-            var table = $('#belumImunisasi').DataTable({
-                processing: true,
-                serverSide: true,
-                responsive: true,
-                language: { url: "//cdn.datatables.net/plug-ins/1.11.4/i18n/id.json" },
-                ajax: window.location.href,
-                lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "Semua"]],
-                columns: [
-                    {data: 'DT_RowIndex', name: 'DT_RowIndex'},
-                    {data: 'nama_lengkap', name: 'nama_lengkap'},
-                    {data: 'tanggal_lahir', name: 'usia',
-                        render: function ( data, type, row, meta ){
-                            let tahun = (new Date().getFullYear()) - (new Date(data).getFullYear());
-                            let bulan = (new Date().getMonth()) - (new Date(data).getMonth());
-                            if(bulan < 0)
-                                return (tahun-1) + " tahun " + (bulan+12) + " bulan";
-                            else if(bulan === 0)
-                                return tahun + " tahun " + bulan + " bulan";
-                            else
-                                return tahun + " tahun " + (bulan+12) + " bulan";
-                        },
-                    },
-                    {data: 'alamat', name: 'alamat'},
-                    {data: 'no_hp', name: 'no_hp'},
-                    {data: 'nama_posyandu', name: 'nama_posyandu'},
-                    {data: 'status', name: 'status'}
-                ],
-                dom: 'Bfrtip',
-                buttons: {
-                    buttons: [
-                    {
-                        extend: 'print',
-                        autoPrint: true,
-                        title: 'Laporan Imunisasi Anak - ',
-                        customize: function ( win ) {
-                            $(win.document.body).find( 'table' )
-                                .addClass( 'compact' )
-                                .css( 'font-size', 'inherit' );
+                $(function () {
+                    var table = $('#belumImunisasi').DataTable({
+                        processing: true,
+                        serverSide: true,
+                        responsive: true,
+                        language: { url: "//cdn.datatables.net/plug-ins/1.11.4/i18n/id.json" },
+                        ajax: window.location.href,
+                        lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "Semua"]],
+                        columns: [
+                            {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                            {data: 'nama_lengkap', name: 'nama_lengkap'},
+                            {data: 'tanggal_lahir', name: 'usia',
+                                render: function ( data, type, row, meta ){
+                                    let tahun = (new Date().getFullYear()) - (new Date(data).getFullYear());
+                                    let bulan = (new Date().getMonth()) - (new Date(data).getMonth());
+                                    if(bulan < 0)
+                                        return (tahun-1) + " tahun " + (bulan+12) + " bulan";
+                                    else if(bulan === 0)
+                                        return tahun + " tahun " + bulan + " bulan";
+                                    else
+                                        return tahun + " tahun " + (bulan+12) + " bulan";
+                                },
+                            },
+                            {data: 'alamat', name: 'alamat'},
+                            {data: 'no_hp', name: 'no_hp'},
+                            {data: 'nama_posyandu', name: 'nama_posyandu'},
+                            {data: 'status', name: 'status'}
+                        ],
+                        dom: 'Bfrtip',
+                        buttons: {
+                            buttons: [
+                                {
+                                    extend: 'print',
+                                    autoPrint: true,
+                                    title: 'Laporan Imunisasi Anak - ' + nama,
+                                    customize: function ( win ) {
+                                        $(win.document.body).find( 'table' )
+                                            .addClass( 'compact' )
+                                            .css( 'font-size', 'inherit' );
+                                    }
+                                },
+                                'pageLength'
+                            ]
                         }
-                    },
-                    'pageLength'
-                ]
-               }
-            });
+                    });
+                });
+            }
         });
     </script>
 @endsection
