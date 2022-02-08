acb9e0b9264c721aa2e062735f37d7ab97613d7b
naufal sunandar <65304518+icedmuffin@users.noreply.github.com>
2022-02-08 12:16:33 +0700

    required login

--- resources/views/umum/login.blade.php
+++ resources/views/umum/login.blade.php
@@ -44,11 +44,11 @@
                             @csrf
                             <div class="form-group">
                                 <label for="username" class="login-label">Username :</label>
-                            <input type="text" class="form-control" id="username" name="username" aria-describedby="username">
+                            <input type="text" class="form-control" id="username" name="username" aria-describedby="username" required>
                             </div>
                             <div class="form-group">
                                 <label for="password" class="login-label">Password :</label>
-                                <input type="password" class="form-control" id="password" name="password">
+                                <input type="password" class="form-control" id="password" name="password" required>
                             </div>
                             <div class="form-group">
                                 <div class="row justify-content-center">
