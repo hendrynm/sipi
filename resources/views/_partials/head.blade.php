bf6ac463f68639cb587c96588cf8ebf63d2736aa
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2022-02-11 23:24:58 +0700

    url

--- resources/views/_partials/head.blade.php
+++ resources/views/_partials/head.blade.php
@@ -4,22 +4,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield("title") | SIPI Papua Barat</title>
 
-    <link rel="apple-touch-icon" sizes="57x57" href="{{asset("images/icons/apple-icon-57x57.png")}}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{asset("images/icons/apple-icon-60x60.png")}}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{asset("images/icons/apple-icon-72x72.png")}}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{asset("images/icons/apple-icon-76x76.png")}}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{asset("images/icons/apple-icon-114x114.png")}}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{asset("images/icons/apple-icon-120x120.png")}}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{asset("images/icons/apple-icon-144x144.png")}}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{asset("images/icons/apple-icon-152x152.png")}}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset("images/icons/apple-icon-180x180.png")}}">
-    <link rel="icon" type="image/png" sizes="192x192"  href="{{asset("images/icons/android-icon-192x192.png")}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset("images/icons/favicon-32x32.png")}}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{asset("images/icons/favicon-96x96.png")}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset("images/icons/favicon-16x16.png")}}">
-    <link rel="manifest" href="{{asset("images/icons/manifest.json")}}">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{url("images/icons/apple-icon-57x57.png")}}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{url("images/icons/apple-icon-60x60.png")}}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{url("images/icons/apple-icon-72x72.png")}}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{url("images/icons/apple-icon-76x76.png")}}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{url("images/icons/apple-icon-114x114.png")}}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{url("images/icons/apple-icon-120x120.png")}}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{url("images/icons/apple-icon-144x144.png")}}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{url("images/icons/apple-icon-152x152.png")}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{url("images/icons/apple-icon-180x180.png")}}">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{url("images/icons/android-icon-192x192.png")}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{url("images/icons/favicon-32x32.png")}}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{url("images/icons/favicon-96x96.png")}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{url("images/icons/favicon-16x16.png")}}">
+    <link rel="manifest" href="{{url("images/icons/manifest.json")}}">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="{{asset("images/icons/ms-icon-144x144.png")}}">
+    <meta name="msapplication-TileImage" content="{{url("images/icons/ms-icon-144x144.png")}}">
     <meta name="theme-color" content="#ffffff">
 
     <!-- css boostrap v4.6 -->
