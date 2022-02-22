a13051db5d3f6fff1dff9eff7f4d73546be64b95
naufal sunandar <65304518+icedmuffin@users.noreply.github.com>
2022-02-22 09:12:25 +0700

    typo

--- resources/views/puskesmas/dataIndividu/dashboard.blade.php
+++ resources/views/puskesmas/dataIndividu/dashboard.blade.php
@@ -25,7 +25,7 @@
 
         <a href="../dashboard" class="btn btn-primary">back</a>
         <hr>
-        <h1>Data Induvidu</h1>
+        <h1>Data Individu</h1>
         <br>
         <a href="./tambah" class="btn btn-primary">Tambah Data</a>
         <div class="jumbotron custom-table">
