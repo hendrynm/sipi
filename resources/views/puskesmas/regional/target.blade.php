07e1f9a677c62c74783085c4087f55f89c2a3249
Hendry Naufal Marbella <hendrynm@users.noreply.github.com>
2022-04-01 05:40:14 +0700

    Fix typo in view sasaran

--- resources/views/puskesmas/regional/target.blade.php
+++ resources/views/puskesmas/regional/target.blade.php
@@ -230,12 +230,12 @@
                         <tr>
                             <th scope="row">1</th>
                             <td>sedang hamil</td>
-                            <td>{{ $data->wus_tidak_hamil }}</td>
+                            <td>{{ $data->wus_hamil }}</td>
                         </tr>
                         <tr>
                             <th scope="row">2</th>
                             <td>tidak hamil</td>
-                            <td>{{ $data->wus_hamil }}</td>
+                            <td>{{ $data->wus_tidak_hamil }}</td>
                         </tr>
                         </tbody>
                     </table>
